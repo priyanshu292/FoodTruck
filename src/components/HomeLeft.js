ff3b505688ffb8da3d742b5d825f067ac36658a9
Priyanshu Dhoke <dhoke.priyanshu2002@gmail.com>
2024-05-06 21:14:44 +0530

    Full Responsive

--- src/components/HomeLeft.js
+++ src/components/HomeLeft.js
@@ -10,7 +10,7 @@ const HomeLeft = () => {
       <h1 className='text'>Discover the <span className='text-red-500'>Best</span> Food and Drinks</h1>
       <p className='para'>Naturally made Healthcare Products for the better care & support of your body.</p>
       <div className=''>
-        <button className='w-48 h-16 ml-16 mt-7 border border-none bg-red-500 text-white font-bold rounded-full md:mt-12 md:ml-32'>Explore Now!</button>
+        <button className='w-48 h-16 ml-10 mt-7 border border-none bg-red-500 text-white font-bold rounded-full md:mt-12 md:ml-32'>Explore Now!</button>
       </div>
     </div>
   )
