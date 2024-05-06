732be976b7a5cc9122113948a08cfd241e71cc17
Priyanshu Dhoke <dhoke.priyanshu2002@gmail.com>
2024-05-06 20:31:00 +0530

    footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,48 @@
-import React from 'react'
-import foodtruck from "../assets/foodtruck.png"
-import { FaInstagram } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
-import { PiCopyright } from "react-icons/pi";
-
-
+import React from 'react';
+import foodtruck from "../assets/foodtruck.png";
+import { FaInstagram, FaTwitter, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='flex mt-40 bg-gray-100'>
-      <div>
-        <img className="w-40 h-32 m-24" alt='logo' src={foodtruck} />
-      </div>
-      <div className='pt-16 pl-20'>
-        <h3 className='pb-4 font-bold text-blue-800'>Contact Us</h3>
-        <p className='text-gray-500'>Lorem Ipsum Pvt Ltd.5/1, Magalton</p>
-        <p className='text-gray-500'>Road, Phartosh Gate near YTM</p>
-        <p className='text-gray-500'>Market, XYZ-343434</p>
-        <p className='pt-4 text-gray-500'>example2020@gmail.com</p>
-        <p className='pt-4 text-gray-500'>(904) 443-0343</p>
-      </div>
-      <div className='pt-16 pl-40'>
-        <h3 className='pb-4 font-bold text-blue-800'>More</h3>
-        <p className='text-gray-500'>About Us</p>
-        <p className='pt-4 text-gray-500'>Products</p>
-        <p className='pt-4 text-gray-500'>Career</p>
-        <p className='pt-4 text-gray-500'>Contact Us</p>
-      </div>
-      <div>
-        <h3 className='pt-16 pl-80 font-bold text-blue-800'>Social Links</h3>
-        <div className='text-blue-800 pt-4 pl-72 flex'>
-            <FaInstagram className='m-4' />
-            <FaTwitter className='m-4' />
-            <FaFacebookF className='m-4' />
+    <div className="bg-gray-100 py-8 px-4 md:px-8 mt-32">
+      <div className="flex flex-wrap justify-between max-w-6xl mx-auto mt-10 mb-10">
+        {/* Logo */}
+        <div className="w-full md:w-1/4 mb-4 md:mb-0">
+          <img className="w-32 mx-auto md:mx-0" alt='logo' src={foodtruck} />
         </div>
-        <p className='text-gray-500 mt-16 ml-72 flex'><PiCopyright />2022 Food Truck Example</p>
+        
+        {/* Contact */}
+        <div className=" ml-10 mt-8 md:ml-0 md:mt-0 w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-blue-800 font-bold mb-2">Contact Us</h3>
+          <p className="text-gray-500">Lorem Ipsum Pvt Ltd.5/1, Magalton</p>
+          <p className="text-gray-500">Road, Phartosh Gate near YTM</p>
+          <p className="text-gray-500">Market, XYZ-343434</p>
+          <p className="text-gray-500 mt-2">example2020@gmail.com</p>
+          <p className="text-gray-500 mt-2">(904) 443-0343</p>
+        </div>
+        
+        {/* More */}
+        <div className=" ml-10 mt-8 md:ml-0 md:mt-0 w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-blue-800 font-bold mb-2">More</h3>
+          <p className="text-gray-500">About Us</p>
+          <p className="text-gray-500 mt-2">Products</p>
+          <p className="text-gray-500 mt-2">Career</p>
+          <p className="text-gray-500 mt-2">Contact Us</p>
+        </div>
+        
+        {/* Social Links */}
+        <div className=" ml-10 mt-8 md:ml-0 md:mt-0 w-full md:w-1/4 mb-4 md:mb-0">
+          <h3 className="text-blue-800 font-bold mb-2 hidden md:block">Social Links</h3>
+          <div className="flex items-center text-blue-800 mt-4">
+            <FaInstagram className="mr-4" />
+            <FaTwitter className="mr-4" />
+            <FaFacebookF className="mr-4" />
+          </div>
+          <p className="text-gray-500 mt-20">&copy; 2022 Food Truck Example</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
